e2acb8cb62062bf278ead9b46b23993b4d6800e8
Takahashi-11 <r.takahashi.sys19@morijyobi.ac.jp>
2020-06-18 11:52:25 +0900

    add index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <? php
     $dsn = 'mysql:dbname=user;host=127.0.0.1;';
-    $user = 'otsubo';
-    $password = 'morijyobi';
+    $user = 'rikako';
+    $password = 'Morijyobi22';
     try {
         $dbh = new PDO($dsn, $user, $password);
 
         $sql = "select * from rikako";
-        $result = 
+        $result = "";
 
         echo "接続成功\n";
     } catch (PDOException $e) {
